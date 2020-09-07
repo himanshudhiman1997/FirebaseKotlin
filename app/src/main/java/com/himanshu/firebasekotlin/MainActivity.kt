ad7dbfa41e7c4f20713bc41b43f850edf1dbe12f
HIMANSHU DHIMAN <himanshudhimanhr@gmail.com>
2020-09-07 23:10:22 +0530

    first commit

---
+++ app/src/main/java/com/himanshu/firebasekotlin/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.himanshu.firebasekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getToken()
+
+    }
+
+    private fun getToken() {
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+                Log.d("fcm_token", token.toString())
+                Toast.makeText(baseContext, token.toString(), Toast.LENGTH_SHORT).show()
+            })
+    }
+}
